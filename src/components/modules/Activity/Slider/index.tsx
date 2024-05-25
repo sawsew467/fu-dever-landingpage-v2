0ed71bd50741acc64268dbc4f3ff78e055f3f628
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2024-05-25 17:26:05 +0700

    xoa imgae gallery

--- src/components/modules/Activity/Slider/index.tsx
+++ src/components/modules/Activity/Slider/index.tsx
@@ -1,7 +1,4 @@
 "use client";
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
-import "./styles.css";
 
 function Slider({ images }: any) {
   const newImages = images?.map((image: any) => ({
